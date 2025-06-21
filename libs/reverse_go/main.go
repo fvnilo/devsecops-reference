c59fac87e23a14ef94943e70da7ec7a83dc816ce
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2025-06-21 00:03:41 -0400

    Check-in go.work File
    As this is a mono-repo, we accept the go.work file.

--- libs/reverse_go/main.go
+++ libs/reverse_go/main.go
@@ -1,9 +1,10 @@
 package reverse_go
 
 import (
-    "golang.org/x/example/hello/reverse"
+	"fmt"
+  "golang.org/x/example/hello/reverse"
 )
 
 func String(input string) string {
-	return reverse.String(input)
+	return fmt.Sprintf("==> %s <==", reverse.String(input))
 }
